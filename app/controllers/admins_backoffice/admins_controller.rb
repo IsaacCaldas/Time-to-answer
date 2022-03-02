778aa006430ec7940cbf1651702c79d9e0673c88
Isaac Tavares Caldas Gonçalves <isaaccaldasgoncalves@gmail.com>
2022-03-02 09:05:34 -0300

    Partial and kaminari with i18n (pagination)

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -4,7 +4,7 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
   before_action :verify_password, only: [:update]
 
   def index
-    @admins = Admin.all
+    @admins = Admin.all.page(params[:page]).per(5)
   end
 
   def new 
