79d72edbf90989f1be1601e2bc80cbbc48d4f085
Isaac Tavares Caldas Gonçalves <isaaccaldasgoncalves@gmail.com>
2022-03-05 15:35:01 -0300

    user profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  # validations
+  validates :first_name, presence: true, length: { minimum: 2 }, on: :update
+
+  # Virtual attributes  
   def full_name
     [self.first_name, self.last_name].join(' ')
   end
+
 end
