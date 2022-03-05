c1c6be2646f4798e597bfb40cc4fb067d7b519b8
Isaac Tavares Caldas Gonçalves <isaaccaldasgoncalves@gmail.com>
2022-03-05 07:54:07 -0300

    Subjects filter

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -1,2 +1,16 @@
 module SiteHelper
+
+  def msg_jumbotron
+    
+    case params[:action]
+
+    when 'index'
+      "Últimas perguntas cadastradas"
+    when 'questions'
+      "Resultados para \"#{params[:term]}\""
+    when 'subject'
+      "Mostrandos questões para o assunto \"#{params[:subject]}\""
+    end  
+  end
+
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -4,12 +4,17 @@ class Question < ApplicationRecord
   accepts_nested_attributes_for :answers, reject_if: :all_blank, allow_destroy: true 
 
   # Scopes: are searchs in DB
+
+  scope :_search_subject_, ->(page, subject_id){
+    includes(:answers, :subject).where(subject_id: subject_id).page(page).per(15)
+  }
+
   scope :_search_, ->(page, term){
-    includes(:answers).where("lower(description) LIKE ?", "%#{term.downcase}%").page(page).per(15)
+    includes(:answers, :subject).where("lower(description) LIKE ?", "%#{term.downcase}%").page(page).per(15)
   }
 
   scope :_last_questions_, ->(page){
-    includes(:answers).order('created_at desc').page(page).per(5)
+    includes(:answers, :subject).order('created_at desc').page(page).per(5)
   }
 
 end
