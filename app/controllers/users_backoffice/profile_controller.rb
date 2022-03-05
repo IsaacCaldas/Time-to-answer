2e5ebbfc83c6df794df417ea440345164d83060d
Isaac Tavares Caldas Gonçalves <isaaccaldasgoncalves@gmail.com>
2022-03-05 18:41:17 -0300

    Modal window - avatar image  - extra data

--- app/controllers/users_backoffice/profile_controller.rb
+++ app/controllers/users_backoffice/profile_controller.rb
@@ -7,11 +7,16 @@ class UsersBackoffice::ProfileController < UsersBackofficeController
     @user.build_user_profile if @user.user_profile.blank?
   end
 
-  def update 
+  def update
     if @user.update(params_user)
       bypass_sign_in(@user)
-      redirect_to users_backoffice_profile_path, notice: "Updated successfuly!"
-    else 
+      if params_user[:user_profile_attributes][:avatar]
+        redirect_to users_backoffice_welcome_index_path, notice: "Avatar atualizado com
+      sucesso!"
+      else
+        redirect_to users_backoffice_profile_path, notice: "Usuário atualizado com sucesso!"
+      end
+    else
       render :edit
     end
   end
@@ -30,7 +35,7 @@ class UsersBackoffice::ProfileController < UsersBackofficeController
 
   def params_user
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation,
-    user_profile_attributes: [:id, :address, :gender, :birthdate])
+    user_profile_attributes: [:id, :address, :gender, :birthdate, :avatar])
   end
 
 end
