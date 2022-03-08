6c98412196e5e6054f9e99b254f9ca79157a91f9
Isaac Tavares Caldas Gonçalves <isaaccaldasgoncalves@gmail.com>
2022-03-07 21:05:23 -0300

    Update to master branch

--- app/controllers/site/answer_controller.rb
+++ app/controllers/site/answer_controller.rb
@@ -2,7 +2,7 @@ class Site::AnswerController < SiteController
 
   def question
     #@answer = Answer.find(params[:answer_id])
-    redis_answer = Redis.cache.read(params[:answer_id]).split("@@")
+    redis_answer = Rails.cache.read(params[:answer_id]).split("@@")
     @question_id = redis_answer.first
     @correct = ActiveModel::Type::Boolean.new.cast(redis_answer.last)
 
